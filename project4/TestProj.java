9fea7a4f8771296d5b0d55c68c81f81e59a086a9
bigdiggysquiggle <randomcouchpotato@gmail.com>
2022-07-06 11:53:41 -0700

    turns out you need to initialize things you're planning to use

--- project4/TestProj.java
+++ project4/TestProj.java
@@ -3,18 +3,18 @@ import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
 public class TestProj	{
-	Cave cave;
-	GuessTheNumber num;
 	@Test
-
 	void	runGames()	{
+		Game[] games = new Game[2];
+		games[0]  = new Cave();
+		games[1] = new GuessTheNumber();
 		ByteArrayInputStream in = new ByteArrayInputStream("4\n9\n1\n1\n1\n1\n1\n1\n1\n1\n1\n2\n1\n2\n1\n2\n1\n2\n2\n10\n5\n15\n20\n-12\n60\n12\n6\n".getBytes());
 		System.setIn(in);
 		Scanner test = new Scanner(System.in);
 		for (int i = 0; i < 19; i++)
-			cave.Play(test);
-		for (int i = 19; i < 27; i++)
-			num.Play(test);
+			games[0].Play(test);
+//		for (int i = 19; i < 27; i++)
+		games[1].Play(test);
 	}
 	public static void main(String[] args)	{
 		TestProj test = new TestProj();
