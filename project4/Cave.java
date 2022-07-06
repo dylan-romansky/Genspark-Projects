51351e3dccb3ad8143885d2de20f7744d6098ca9
bigdiggysquiggle <randomcouchpotato@gmail.com>
2022-07-06 14:00:00 -0700

    realized a bug exists

--- project4/Cave.java
+++ project4/Cave.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.InputMismatchException;
+import java.lang.Exception;
 
 public class Cave extends Game	{
 	String cave_app = "You approach the cave...\nIt is dark and spooky...\nA large dragon jumps out in front of you! He opens his jaws and...\n";
 	String[] dragons = {"Gobbles you down in one bite", "Offers you some of his treasure..."};
 
-	private void _caveGame(int input)	{
+	private void _caveGame(int input) throws Exception	{
 
 		//give the player the illusion of choice
 		Random rand = new Random(System.currentTimeMillis() / 1000L);
 		input += rand.nextInt();
+		if (!(0 < input && input < 2)){
+			throw(new Exception("Error: invalid input"));
+		}
 		input &= 1;
 		System.out.println(cave_app + dragons[input]);
 	}
@@ -21,7 +25,7 @@ public class Cave extends Game	{
 		try	{
 			_caveGame(input.nextInt());
 		}
-		catch(InputMismatchException e)	{
+		catch(Exception e)	{
 			System.out.println("Error, invalid input");
 		}
 	}
