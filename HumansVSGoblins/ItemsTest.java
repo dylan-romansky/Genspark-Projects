0ad1a85e326f66fd21dcf37d20795174d15bd6e3
bigdiggysquiggle <randomcouchpotato@gmail.com>
2022-07-13 06:43:54 -0700

    item generation works

---
+++ HumansVSGoblins/ItemsTest.java
@@ -0,0 +1,51 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+class ItemsTest  {
+    @Test
+    public void weaponGen() {
+        Sword sord = new Sword();
+        Axe ax = new Axe();
+        Spear sper = new Spear();
+        assertEquals(0, "Sword".compareTo(sord.toString()));
+        assertEquals(0, "Axe".compareTo(ax.toString()));
+        assertEquals(0, "Spear".compareTo(sper.toString()));
+        Weapon wep;
+        try {
+            for (int i = 0; i < 7; i++) {
+                wep = Weapon.createWeapon();
+                System.out.println(wep.toString());
+                assertTrue(wep.getMod() > 0);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+            fail();
+        }
+    }
+    @Test
+    public void consumableGen() {
+        SmallPotion smPot = new SmallPotion();
+        Potion pot = new Potion();
+        LargePotion lgPot = new LargePotion();
+        assertEquals(0, "Small Potion".compareTo(smPot.toString()));
+        assertEquals(0, "Potion".compareTo(pot.toString()));
+        assertEquals(0, "Large Potion".compareTo(lgPot.toString()));
+        Consumable yum;
+        try {
+            for (int i = 0; i < 7; i++) {
+                yum = Consumable.makeConsumable();
+                System.out.println(yum.toString());
+                assertTrue(yum.getMod() > 0);
+            }
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+    public static void main(String[] args)  {
+        ItemsTest test = new ItemsTest();
+        test.weaponGen();
+        test.consumableGen();
+    }
+}
