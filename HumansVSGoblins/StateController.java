652655c213f60870ab6b1139006f49bcfb817d8b
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-07-27 13:59:04 -0700

    implementing menuing

--- HumansVSGoblins/StateController.java
+++ HumansVSGoblins/StateController.java
@@ -15,6 +15,7 @@ public class StateController {
         int xbound = 10;
         int ybound = 10;
         fighter = new Human(new Coordinates(xbound/2, ybound/2), "Greg");
+        panel.setFighter(fighter);
         for (int i = 0; i <= 2; i++) {
             int x = xbound/2;
             int y = ybound/2;
@@ -31,6 +32,7 @@ public class StateController {
         panel = new Video(x, y);
         terra = new Terrain(x, y, panel);
         fighter = new Human(new Coordinates(x/2, y/2), panel.setup());
+        panel.setFighter(fighter);
         for (int i = 0; i <= 2; i++)    {
             int _x = x/2;
             int _y = y/2;
@@ -83,12 +85,15 @@ public class StateController {
         hominids.removeAll(combatants);
         while (combatants.size() > 0) {
             double nextUpdate = System.nanoTime() + refreshRate;
-            if ((last != panel.direct && panel.direct != Video.directions.NONE) || panel.click == true)
+            if ((last != panel.direct && panel.direct != Video.directions.NONE) || panel.click == true || panel.back == true)
                 panel.updateFight();
             last = panel.direct;
             panel.draw();
-            if (fighter.getHealth() <= 0)
+            if (fighter.getHealth() <= 0) {
+                panel.death();
                 playing = false;
+                return;
+            }
             try {
                 long wait = (long) (nextUpdate - System.nanoTime()) / 1000000;
                 Thread.sleep(wait < 0 ? 0 : wait);  }
@@ -104,29 +109,35 @@ public class StateController {
         Video.directions last = Video.directions.NONE;
         while (playing) {
             double nextUpdate = System.nanoTime() + refreshRate;
-            if (panel.direct != Video.directions.NONE && panel.direct != last) { //kinda hacky but it keeps the game responsive while also staying turn based
-                switch (panel.direct) {
-                    case UP -> fighter.updateCoords('w', terra.getX(), terra.getY());
-                    case DOWN -> fighter.updateCoords('s', terra.getX(), terra.getY());
-                    case LEFT -> fighter.updateCoords('a', terra.getX(), terra.getY());
-                    case RIGHT -> fighter.updateCoords('d', terra.getX(), terra.getY());
+            if (panel.click && !panel.hold)
+                panel.menuing = true;
+            else if (!panel.menuing)    {
+                if (panel.direct != Video.directions.NONE && panel.direct != last) { //kinda hacky but it keeps the game responsive while also staying turn based
+                    switch (panel.direct) {
+                        case UP -> fighter.updateCoords('w', terra.getX(), terra.getY());
+                        case DOWN -> fighter.updateCoords('s', terra.getX(), terra.getY());
+                        case LEFT -> fighter.updateCoords('a', terra.getX(), terra.getY());
+                        case RIGHT -> fighter.updateCoords('d', terra.getX(), terra.getY());
+                    }
+                    for (Humanoid dude : hominids)
+                        dude.updateCoords(terra.getX(), terra.getY());
+                    fightloop(fightCheck());
                 }
-                for (Humanoid dude : hominids)
-                    dude.updateCoords(terra.getX(), terra.getY());
-                fightloop(fightCheck());
-            }
-            if (_rng.nextInt() % 10 == 0 && hominids.size() < 5) {
-                Coordinates playerPos = fighter.getCoords();
-                int x = playerPos.getX() / 2;
-                int y = playerPos.getY() / 2;
-                while (x == playerPos.getX() / 2 && y == playerPos.getY() / 2) {
-                    x = _rng.nextInt(0, terra.getX());
-                    y = _rng.nextInt(0, terra.getY());
+                if (_rng.nextInt() % 10 == 0 && hominids.size() < 5) {
+                    Coordinates playerPos = fighter.getCoords();
+                    int x = playerPos.getX() / 2;
+                    int y = playerPos.getY() / 2;
+                    while (x == playerPos.getX() / 2 && y == playerPos.getY() / 2) {
+                        x = _rng.nextInt(0, terra.getX());
+                        y = _rng.nextInt(0, terra.getY());
+                    }
+                    hominids.add(new Goblin(new Coordinates(x, y)));
                 }
-                hominids.add(new Goblin(new Coordinates(x, y)));
+                last = panel.direct;
+                terra.update(fighter, hominids);
             }
-            last = panel.direct;
-            terra.update(fighter, hominids);
+            else
+                panel.mainMenuControl();
             terra.draw();
             try {
                 long wait = (long) (nextUpdate - System.nanoTime()) / 1000000;
@@ -134,6 +145,37 @@ public class StateController {
             catch (Exception e) {
                 e.printStackTrace();
             }
+            if (!playing) {
+                replayCheck();
+                System.out.println(playing);
+            }
         }
+    }
+    private void replayCheck()  {
+        System.out.println("so we here then?");
+        System.out.println(panel.click + " && " + panel.back);
+        while (panel.hold)
+            System.out.println("this sucks");
+        while (!panel.click && !panel.back)
+            System.out.println("waiting");
+        playing = panel.click;
+        if (!playing)
+            return;
+        Coordinates fightcoord = fighter.getCoords();
+        int x = terra.getX();
+        int y = terra.getY();;
+        fightcoord.set(x/2, y/2);
+        fighter.setHealth(100);
+        hominids.removeAll(hominids);
+        for (int i = 0; i <= 2; i++)    {
+            int _x = x/2;
+            int _y = y/2;
+            while (_x == x/2 && _y == y/2)    {
+                _x = _rng.nextInt(0, x);
+                _y = _rng.nextInt(0, y);
+            }
+            hominids.add(new Goblin(new Coordinates(_x, _y)));
+        }
+        panel.reset();
     }
 }
--- HumansVSGoblins/Video.java
+++ HumansVSGoblins/Video.java
@@ -12,8 +12,11 @@ public class Video extends JPanel implements KeyListener {
     private boolean fighting = false;
     boolean click = false;
     boolean back = false;
-    private boolean hold = false;
+    boolean hold = false;
     public fightState fight;
+    private boolean dead = false;
+    public menuState menu;
+    boolean menuing = false;
     Video(int x, int y)  {
         setPreferredSize(new Dimension(x * 50, y * 50));
         setBackground(Color.black);
@@ -21,9 +24,13 @@ public class Video extends JPanel implements KeyListener {
         addKeyListener(this);
         setFocusable(true);
         fight = new fightState(x * 50, (y * 50) / 2);
+        menu = new menuState(x * 50, y * 50);
     }
     void setGrid(ArrayList<Terrain.tile> g) {
         grid = g;
+    }
+    void setFighter(Human fig)  {
+        menu.fighter = fig;
     }
     @Override
     public void keyTyped(KeyEvent e) {
@@ -94,6 +101,27 @@ public class Video extends JPanel implements KeyListener {
     public void draw()  {
         repaint();
     }
+    public void death() {
+        dead = true;
+        fightToggle();
+    }
+    public void reset() {
+        dead = false;
+        click = false;
+        back = false;
+        hold = false;
+        direct = directions.NONE;
+    }
+    public void death(Graphics2D g2)    {
+        g2.setColor(Color.black);
+        g2.fillRect(50, 50, fight.width - 100, fight.height - 50);
+        g2.setColor(Color.white);
+        g2.setStroke(new BasicStroke(5));
+        g2.drawRect(50, 50, fight.width - 100, fight.height - 50);
+        g2.drawString("Game Over", fight.width/2 - 5*6, fight.height/2);
+        g2.drawString("Play Again?", fight.width/2 - 5*6, fight.height/2 + 15);
+        g2.drawString("Enter: play      Esc: exit", fight.width/2 - 11*6, fight.height/2 + 40);
+    }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
@@ -101,6 +129,10 @@ public class Video extends JPanel implements KeyListener {
             t.draw(g2);
         if (fighting)
             fight.draw(g2);
+        if (menuing)
+            menu.menstat(g2);
+        if (dead)
+            death(g2);
         g2.dispose();
     }
     private class fightState {
@@ -110,7 +142,7 @@ public class Video extends JPanel implements KeyListener {
         int height;
         int state = 0;
         String[] menuOpts = {"Attack", "Inventory"};
-        int selOpt = 0;
+        int selOpt = 1;
         int choGob = 0;
         int chosIt = 0;
         Loot lootPool;
@@ -148,14 +180,14 @@ public class Video extends JPanel implements KeyListener {
         }
         public void drawInv(Graphics2D g2){
             g2.setColor(Color.black);
-            g2.fillRect(width/3, 25, width/3, height-25);
+            g2.fillRect(width/3, 25, width/3, 2*height-50);
             g2.setColor(Color.white);
-            g2.drawRect(width/3, 25, width/3, height-25);
+            g2.drawRect(width/3, 25, width/3, 2*height-50);
             int x = (width / 3) + 25;
             int y = 50;
             String[] set = fighter.getInv().conString();
-            for (int i = 0; i < set.length; i ++) {
-                g2.drawString(set[i], x, y);
+            for (String s : set) {
+                g2.drawString(s, x, y);
                 y += 20;
             }
             g2.drawString("close", x, y);
@@ -166,25 +198,143 @@ public class Video extends JPanel implements KeyListener {
             combatants = co;
         }
     }
+    private class menuState {
+        String[] opts = { "Equipment", "Items", "Player", "close" };
+        int width, height;
+        int mainOpt = 0;
+        int subOpt = 0;
+        int state = 0;
+        Human fighter;
+        boolean inspect = false;
+        int inspecting = 0;
+        menuState(int x, int y) {
+            width = x;
+            height = y;
+        }
+        public void menstat(Graphics2D g2)  {
+            switch(state)   {
+                case 0 -> drawMainMenu(g2);
+                case 1 -> drawEquipMenu(g2);
+                case 2 -> drawItMenu(g2);
+                case 3 -> drawPlayerStat(g2);
+            }
+        }
+        public void drawMainMenu(Graphics2D g2) {
+            int x = width/2;
+            g2.setColor(Color.black);
+            g2.fillRect(x, 0, width/2, 0);
+            g2.setColor(Color.white);
+            g2.setStroke(new BasicStroke(5));
+            g2.drawRect(x, 0, width/2, 0);
+            int y = 5;
+            x += 5;
+            for (String opt : opts) {
+                g2.drawString(opt, x, y);
+                y += 15;
+            }
+            g2.drawLine(x,7 + (mainOpt * 15), getFontMetrics(getFont()).stringWidth(mainOpt < opts.length ? opts[mainOpt] : "close"), 7 + (mainOpt * 15));
+        }
+        public void drawEquipMenu(Graphics2D g2)   {
+            int x = width/2;
+            g2.setColor(Color.black);
+            g2.fillRect(0, 0, width, height);
+            g2.setStroke(new BasicStroke(5));
+            g2.drawRect(0, 0, x, height);
+            g2.drawRect(x, 0, x, height);
+            int y = 15;
+            g2.drawString(fighter.toString(), 5, y);
+            g2.drawString("HP: " + fighter.getHealth(), 5, y + 10);
+            g2.drawString("Weapon: " + fighter.getWep() + ", +" + fighter.getWep().getMod() + " dmg", 5, y + 20);
+            g2.drawString("Attack: " + fighter.getAttack(), 5, y + 30);
+            g2.drawString("Dmg Total: " + (fighter.getAttack() + fighter.getWep().getMod()), 5, 40);
+            for (Weapon wep : fighter.getInv().getWeapons())    {
+                g2.drawString(wep.toString() + ", +" + wep.getMod() + " dmg", x + 5, y);
+                y += 10;
+            }
+            g2.drawLine(x + 5,17 + (subOpt * 10), getFontMetrics(getFont()).stringWidth(mainOpt < opts.length ? opts[mainOpt] : "close"), 17 + (subOpt * 10));
+            if (inspect)
+                inspection(g2);
+        }
+        public void drawItMenu(Graphics2D g2)   {
+            int x = width/2;
+            g2.setColor(Color.black);
+            g2.fillRect(0, 0, width, height);
+            g2.setStroke(new BasicStroke(5));
+            g2.drawRect(0, 0, x, height);
+            g2.drawRect(x, 0, x, height);
+            int y = 15;
+            g2.drawString(fighter.toString(), 5, y);
+            g2.drawString("HP: " + fighter.getHealth(), 5, y + 10);
+            g2.drawString("Weapon: " + fighter.getWep() + ", +" + fighter.getWep().getMod() + " dmg", 5, y + 20);
+            g2.drawString("Attack: " + fighter.getAttack(), 5, y + 30);
+            g2.drawString("Dmg Total: " + (fighter.getAttack() + fighter.getWep().getMod()), 5, 40);
+            for (Consumable con : fighter.getInv().getItems())    {
+                g2.drawString(con.toString() + ", +" + con.getMod() + " hp", x + 5, y);
+                y += 10;
+            }
+            g2.drawLine(x + 5,17 + (subOpt * 10), getFontMetrics(getFont()).stringWidth(mainOpt < opts.length ? opts[mainOpt] : "close"), 17 + (subOpt * 10));
+            if (inspect)
+                inspection(g2);
+        }
+        public void drawPlayerStat(Graphics2D g2) {
+            int x = width / 2;
+            g2.setColor(Color.black);
+            g2.fillRect(0, 0, width, height);
+            g2.setStroke(new BasicStroke(5));
+            g2.drawRect(0, 0, width, height);
+            int y = 15;
+            g2.drawString(fighter.toString(), x, y);
+            g2.drawString("HP: " + fighter.getHealth(), x, y + 10);
+            g2.drawString("Weapon: " + fighter.getWep() + ", +" + fighter.getWep().getMod() + " dmg", x, y + 20);
+            g2.drawString("Attack: " + fighter.getAttack(), x, y + 30);
+            g2.drawString("Dmg Total: " + (fighter.getAttack() + fighter.getWep().getMod()), x, 40);
+        }
+        public void inspection(Graphics2D g2)   {
+            int x = width/2 - 15;
+            int y = height/2 - 15;
+            g2.setColor(Color.black);
+            g2.fillRect(x, y, 30, 30);
+            g2.drawRect(x, y, 30, 30);
+            g2.drawString("Use", x + 10, y + 10);
+            g2.drawString("cancel", x + 5, y + 20);
+            if (inspecting == 0)
+                g2.drawLine(x + 10, y + 10, getFontMetrics(getFont()).stringWidth("Use"), y + 10);
+            else
+                g2.drawLine(x + 5, y + 20, getFontMetrics(getFont()).stringWidth("Cancel"), y + 20);
+        }
+        public void use()   {
+            Inventory inv = fighter.getInv();
+            switch (menu.state) {
+                case 1:
+                    if (subOpt == inv.getWeapons().size()) {
+                        menu.state = 0;
+                        break;
+                    }
+                    inv.getWeapons().add(fighter.getWep());
+                    fighter.setWeapon(inv.getWeapons().get(subOpt));
+                    break;
+                case 2:
+                    if (subOpt == inv.getItems().size()) {
+                        menu.state = 0;
+                        break;
+                    }
+                    fighter.useConsumable(subOpt);
+            }
+            menu.inspect = false;
+        }
+    }
     public void addCombatants(Human hu, ArrayList<Humanoid> co) {
         fight.addCombatants(hu, co);
     }
     public void updateFight()   {
         switch (fight.state) {
-            case 0:
-                menuControl();
-                break;
-            case 1:
-                chooseTarget();
-                break;
-            case 2:
-                chooseItem();
-                break;
+            case 0 -> menuControl();
+            case 1 -> chooseTarget();
+            case 2 -> chooseItem();
         }
     }
 
     private void menuControl()  {
-        System.out.println(click + " && " + !hold);
         if (click && !hold) {
             fight.state += 1 + fight.selOpt;
             hold = true;
@@ -205,7 +355,6 @@ public class Video extends JPanel implements KeyListener {
         }
     }
     public void chooseTarget()  {
-        System.out.println(click + " && " + !hold);
         if (click && !hold) {
             fight.state = 0;
             Humanoid targ = fight.combatants.get(fight.choGob);
@@ -217,6 +366,10 @@ public class Video extends JPanel implements KeyListener {
             for (Humanoid combatant : fight.combatants) {
                 combatant.attack(fight.fighter);
             }
+            hold = true;
+        }
+        if (back && !hold)  {
+            fight.state = 0;
             hold = true;
         }
         switch (direct) {
@@ -235,11 +388,15 @@ public class Video extends JPanel implements KeyListener {
         }
     }
     public void chooseItem()    {
-        System.out.println(click + " && " + !hold);
+        System.out.println(back + " && " + hold);
         if (click && !hold) {
             fight.state = 0;
             if (fight.chosIt < fight.fighter.getInv().getItems().size())
                 fight.fighter.useConsumable(fight.chosIt);
+            hold = true;
+        }
+        if (back && !hold)  {
+            fight.state = 0;
             hold = true;
         }
         switch (direct) {
@@ -265,5 +422,51 @@ public class Video extends JPanel implements KeyListener {
     }
     public void clearLoot() {
         fight.lootPool.emptyPool();
+    }
+    void mainMenuControl()  {
+        if (click && !hold) {
+            if (menu.inspect)
+                menu.use();
+            else if (menu.state != 0)
+                menu.inspect = true;
+            else
+                menu.state += 1 + menu.mainOpt;
+            hold = true;
+        }
+        if (back && !hold)  {
+            if (menu.inspect)
+                menu.inspect = false;
+            else if (menu.state != 0)
+                menu.state = 0;
+            else
+                menuing = false;
+        }
+        switch (direct) {
+            case UP:
+            case RIGHT:
+                if (menu.state == 0)
+                    menu.mainOpt -= menu.mainOpt == 0 ? 0 : 1;
+                else if (menu.inspect)
+                    menu.inspecting = menu.inspecting == 0 ? 1 : 0;
+                else
+                    menu.subOpt -= menu.subOpt == 0 ? 0 : 1;
+                break;
+            case DOWN:
+            case LEFT:
+                if (menu.state == 0)
+                    menu.mainOpt += menu.mainOpt == menu.opts.length - 1 ? 0 : 1;
+                else if (menu.inspect)
+                    menu.inspecting = menu.inspecting == 0 ? 1 : 0;
+                else {
+                    int max;
+                    max = switch (menu.state) {
+                        case 1 -> menu.fighter.getInv().getWeapons().size();
+                        case 2 -> menu.fighter.getInv().getItems().size();
+                        case default -> 0;
+                    };
+                    menu.subOpt += menu.subOpt >= max ? 0 : 1;
+                }
+                break;
+        }
     }
 }
